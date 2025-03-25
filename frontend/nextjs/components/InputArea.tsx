c0c9d71568f7f5df4be40d8b4ff10e3caefd8845
Vadim <96525166+Mopiapti@users.noreply.github.com>
2025-03-25 22:53:30 +0200

    Input field style update

--- frontend/nextjs/components/InputArea.tsx
+++ frontend/nextjs/components/InputArea.tsx
@@ -19,43 +19,41 @@ const InputArea: FC<TInputAreaProps> = ({
 }) => {
   return (
     <form
-    className="mx-auto flex h-[198px] w-full items-stretch justify-between rounded-lg border bg-white px-3 shadow-[2px_2px_38px_0px_rgba(0,0,0,0.25),0px_-2px_4px_0px_rgba(0,0,0,0.25)_inset,1px_2px_4px_0px_rgba(0,0,0,0.25)_inset]"
-    onSubmit={(e) => {
-      e.preventDefault();
-      if (reset) reset();
-      handleDisplayResult();
-    }}
-  >
-    <input
-      type="text"
-      placeholder="What would you like me to research next?"
-      className="h-full w-full px-5 py-3 text-xl font-light leading-normal text-black outline-none focus-visible:ring-0 focus-visible:ring-offset-0"
-      disabled={disabled}
-      value={promptValue}
-      required
-      onChange={(e) => setPromptValue(e.target.value)}
-    />
-    <button
-      disabled={disabled}
-      type="submit"
-      className="relative flex h-[50px] w-[50px] shrink-0 items-center justify-center rounded-[3px] bg-[linear-gradient(154deg,#1B1B16_23.37%,#565646_91.91%)] disabled:pointer-events-none disabled:opacity-75"
+      className="mx-auto flex h-[198px] w-full items-center justify-between rounded-lg border bg-white px-3 shadow-[2px_2px_38px_0px_rgba(0,0,0,0.25),0px_-2px_4px_0px_rgba(0,0,0,0.25)_inset,1px_2px_4px_0px_rgba(0,0,0,0.25)_inset]"
+      onSubmit={(e) => {
+        e.preventDefault();
+        if (reset) reset();
+        handleDisplayResult();
+      }}
     >
-      {disabled && (
-        <div className="absolute inset-0 flex items-center justify-center">
-          <TypeAnimation />
-        </div>
-      )}
-
-      <Image
-        unoptimized
-        src={"/img/arrow-narrow-right.svg"}
-        alt="search"
-        width={24}
-        height={24}
-        className={disabled ? "invisible" : ""}
+      <textarea
+        placeholder="What would you like me to research next?"
+        className="focus-visible::outline-0 my-1 w-full h-[150px] pl-5 font-light not-italic leading-[normal] text-black outline-none resize-none focus-visible:ring-0 focus-visible:ring-offset-0 sm:text-xl"
+        disabled={disabled}
+        value={promptValue}
+        required
+        onChange={(e) => setPromptValue(e.target.value)}
       />
-    </button>
-</form>
+      <button
+        disabled={disabled}
+        type="submit"
+        className="relative flex h-[50px] w-[50px] shrink-0 items-center justify-center rounded-[3px] bg-[linear-gradient(154deg,#1B1B16_23.37%,#565646_91.91%)] disabled:pointer-events-none disabled:opacity-75"
+      >
+        {disabled && (
+          <div className="absolute inset-0 flex items-center justify-center">
+            <TypeAnimation />
+          </div>
+        )}
+        <Image
+          unoptimized
+          src={"/img/arrow-narrow-right.svg"}
+          alt="search"
+          width={24}
+          height={24}
+          className={disabled ? "invisible" : ""}
+        />
+      </button>
+    </form>
   );
 };
 
